080aa8a9d784c69cbbcce78d1dbc1f8a9090e734
Jivebread <Jivebread>
2017-12-19 16:16:18 +0900

    contact page is added on url

--- blogwebsite/views.py
+++ blogwebsite/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request,'blogwebsite/home.html')
+def contact(request):
+	return render(request,'blogwebsite/basic.html',{'content':['if you would like to contact me, please email me','jbnub12@gmail.com']})
